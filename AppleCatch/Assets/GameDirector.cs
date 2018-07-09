9107b70be8f4a4229803dca900da96c14a57903e
Unknown <g1727056@st.t-kougei.ac.jp>
2018-07-09 18:50:24 +0900

    天狐バース

---
+++ AppleCatch/Assets/GameDirector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+
+    GameObject timerText;
+    float time = 60.0f;
+
+    void Start()
+    {
+        this.timerText = GameObject.Find("Time");
+    }
+
+    void Update()
+    {
+        this.time -= Time.deltaTime;
+        this.timerText.GetComponent<Text>().text = 
+            this.time.ToString("F1");
+    }
+}
